1cbd025a9925c098cd3dc8b767d789efc3a47dfd
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-05-05 14:00:16 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -2,15 +2,15 @@
 yes | pkg update && pkg upgrade
 yes | pkg install libjansson build-essential clang binutils git
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
-git clone https://github.com/june4th/ccminer-termux.git
-cd ccminer-termux
+git clone https://github.com/june4th/ccminer.git
+cd ccminer
 chmod +x build.sh configure.sh autogen.sh start.sh
 CXX=clang++ CC=clang ./build.sh
 if [ ! -f ~/.bashrc ]; then
   echo "~/ccminer-termux/start.sh" > ~/.bashrc
 else
-  if ! grep -Fxq "~/ccminer-termux/start.sh" ~/.bashrc; then
-    echo "~/ccminer-termux/start.sh" >> ~/.bashrc
+  if ! grep -Fxq "~/ccminer/start.sh" ~/.bashrc; then
+    echo "~/ccminer/start.sh" >> ~/.bashrc
   fi
 fi
 nano config.json
