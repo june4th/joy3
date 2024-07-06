6ffa513094936a9d112a27ac81118e464482cb32
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-07-06 18:24:41 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -13,6 +13,25 @@ else
   fi
 fi
 CXX=clang++ CC=clang ./build.sh
+device_name=$(getprop ro.product.model)
+config_content="{
+  \"pools\":
+    [{
+      \"name\": \"AUTO-NICEHASH\",
+      \"url\": \"stratum+tcp://verushash.auto.nicehash.com:9200\",
+      \"timeout\": 180,
+      \"disabled\": 0
+    }],
+  \"user\": \"NHbJB5KUkEUhp5pAkUYbera7bfdXWKTgjbNE.${device_name}\",
+  \"pass\": \"x\",
+  \"algo\": \"verus\",
+  \"threads\": 8,
+  \"cpu-priority\": 1,
+  \"cpu-affinity\": -1,
+  \"retry-pause\": 10
+}"
+echo "$config_content" | tee ~/ccminer/config.json
+echo "config.json was updated."
 echo "setup nearly complete."
 echo "Edit the config with \"nano ~/ccminer/config.json\""
 echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
