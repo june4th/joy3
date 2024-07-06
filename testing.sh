bf8cf36904e4e90e15a6b4e62e330038afcaf0ef
Phu Hoa Doan <57792296+june4th@users.noreply.github.com>
2024-07-06 22:07:08 +0700

    Create testing.sh

---
+++ testing.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+device_name=$(getprop ro.product.model)
+echo "Setup CCminer for RIG NAME: $device_name"
+read -p "Enter POOL ADDRESS: " pool_address
+read -p "Enter WALLET ADDRESS: " wallet_address
+yes | pkg update && pkg upgrade
+yes | pkg install libjansson build-essential clang binutils git
+cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
+git clone https://github.com/june4th/ccminer.git
+cd ccminer
+chmod +x build.sh configure.sh autogen.sh start.sh
+if [ ! -f ~/.bashrc ]; then
+  echo "~/ccminer/start.sh" > ~/.bashrc
+else
+  if ! grep -Fxq "~/ccminer/start.sh" ~/.bashrc; then
+    echo "~/ccminer/start.sh" >> ~/.bashrc
+  fi
+fi
+CXX=clang++ CC=clang ./build.sh
+config_content="{\"pools\": [{\"name\": \"AUTO-NICEHASH\",\"url\": \"$pool_address\",\"timeout\": 180,\"disabled\": 0}],\"user\": \"$wallet_address.${device_name}\",\"pass\": \"x\",\"algo\": \"verus\",\"threads\": 8,\"cpu-priority\": 1,\"cpu-affinity\": -1,\"retry-pause\": 10}"
+echo "$config_content" | tee ~/ccminer/config.json
+echo "config.json was updated."
+echo "setup nearly complete."
+echo "Edit the config with \"nano ~/ccminer/config.json\""
+echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
